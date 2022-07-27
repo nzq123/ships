d1f5839d31fad399bb04f0d03a60d2df5017de8b
nzq123 <enzet10@gmail.com>
2022-07-27 15:30:14 +0200

    board works now

--- statki.py
+++ statki.py
@@ -2,10 +2,6 @@
 # statki, komentarz kiedy jest zatopiony, życie/rozmiar
 # strzaly
 # zasady umiejscowie, strzałow, kiedy sie konczy,
-
-# 0 1 2 3
-#A
-#B
 import random
 
 tab = []
@@ -32,100 +28,95 @@ def get_board():
 def shots():
     if 'A0' in tab:
         print('AO')
-#sprawdzic, czy jest na zajetym polu = zrobione
-#sprawdzic, czy jest inny obok = zrobione
-#sprawdzic, czy nie wychodzi za tablice
 
 
-def ships(x, y, size, direct):
-    position_of_letter = LETTERS.find(x.upper())
-
-    if tab[position_of_letter][y] == '# ':
-        res = False
-        return res
-    else:
-        res = True
-        res = ships_y(x, y, size, direct) and ships_x(x, y, size, direct)
-        return res
-    print(ships_y(x, y, size, direct))
-    print(ships_x(x, y, size, direct))
+def is_cord_valid(cord):
+    return 0 <= cord <= 9
 
 
-def ships_y(x, y, size, direct):
-    position_of_letter = LETTERS.find(x.upper())
-    if y == 9:
-        if tab[position_of_letter][y - 1] == '# ':
-            res = False
-        else:
-            tab[position_of_letter][y] = '# '
-            res = True
-            get_board()
-        return res
-    if y < 9:
-        if tab[position_of_letter][y - 1] == '# ' or tab[position_of_letter][y + 1] == '# ':
-            res = False
-        else:
-            tab[position_of_letter][y] = '# '
-            res = True
-            get_board()
-        return res
+def is_pos_taken(x, y):
+    return tab[x][y] == '# '
 
 
-def ships_x(x, y, size, direct):
-    position_of_letter = LETTERS.find(x.upper())
-    if position_of_letter == 9:
-        if tab[position_of_letter - 1][y] == '# ':
-            res = False
-        else:
-            tab[position_of_letter][y] = '# '
-            res = True
-            get_board()
-        return res
-    if position_of_letter < 9:
-        if tab[position_of_letter - 1][y] == '# ' and tab[position_of_letter + 1][y] == '# ':
-            res = False
-        else:
-            tab[position_of_letter][y] = '# '
-            res = True
-            get_board()
-        return res
+def is_pos_valid(x, y):
+    if is_cord_valid(x) is False or is_cord_valid(y) is False:
+        return False
+    if is_pos_taken(x, y) is True:
+        return False
+    if is_cord_valid(x - 1) is True:
+        if is_pos_taken(x - 1, y) is True:
+            return False
+    if is_cord_valid(x + 1) is True:
+        if is_pos_taken(x + 1, y) is True:
+            return False
+    if is_cord_valid(y - 1) is True:
+        if is_pos_taken(x, y - 1) is True:
+            return False
+    if is_cord_valid(y + 1) is True:
+        if is_pos_taken(x, y + 1) is True:
+            return False
+    if is_cord_valid(x - 1) is True and is_cord_valid(y - 1) is True:
+        if is_pos_taken(x - 1, y - 1) is True:
+            return False
+    if is_cord_valid(x + 1) is True and is_cord_valid(y - 1) is True:
+        if is_pos_taken(x + 1, y - 1) is True:
+            return False
+    if is_cord_valid(x + 1) is True and is_cord_valid(y + 1) is True:
+        if is_pos_taken(x + 1, y + 1) is True:
+            return False
+    if is_cord_valid(x - 1) is True and is_cord_valid(y + 1) is True:
+        if is_pos_taken(x - 1, y + 1) is True:
+            return False
+    return True
+
+
+def free_place():
+    for i in range(len(tab)):
+        for j in range(len(tab[0])):
+            if is_pos_valid(i, j):
+                return True
+    return False
 
 
 def get_ships():
-    i = 0
-    while i < 50:
-        x = LETTERS[random.randint(0, 9)]
-        y = random.randint(0, 9)
-        result = ships(x, y, 1, 'up')
-        if result:
-            i += 1
+    for i in range(15):
+        if free_place():
+            x = random.randint(0, 9)
+            y = random.randint(0, 9)
+            while not is_pos_valid(x, y):
+                x = random.randint(0, 9)
+                y = random.randint(0, 9)
+            tab[x][y] = '# '
 
-    i = 0
-    while i < 3:
-        x = LETTERS[random.randint(0, 9)]
-        y = random.randint(0, 9)
-        result = ships(x, y, 2, 'up')
-        if result:
-            i += 1
 
-    i = 0
-    while i < 2:
-        x = LETTERS[random.randint(0, 9)]
-        y = random.randint(0, 9)
-        result = ships(x, y, 3, 'up')
-        if result:
-            i += 1
-
-    i = 0
-    while i < 1:
-        x = LETTERS[random.randint(0, 9)]
-        y = random.randint(0, 9)
-        result = ships(x, y, 4, 'up')
-        if result:
-            i += 1
+    # i = 0
+    # while i < 3:
+    #     x = LETTERS[random.randint(0, 9)]
+    #     y = random.randint(0, 9)
+    #     result = ships(x, y, 2, 'up')
+    #     if result:
+    #         i += 1
+    #
+    # i = 0
+    # while i < 2:
+    #     x = LETTERS[random.randint(0, 9)]
+    #     y = random.randint(0, 9)
+    #     result = ships(x, y, 3, 'up')
+    #     if result:
+    #         i += 1
+    #
+    # i = 0
+    # while i < 1:
+    #     x = LETTERS[random.randint(0, 9)]
+    #     y = random.randint(0, 9)
+    #     result = ships(x, y, 4, 'up')
+    #     if result:
+    #         i += 1
 
 board()
 get_ships()
+get_board()
+free_place()
 #shots()
 
 #ships('j', 5, 1, 'up')
