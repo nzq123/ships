320ecb9078d5bf7ecce1f6e0f140d45695aaceea
nzq123 <enzet10@gmail.com>
2022-07-28 15:36:42 +0200

    ships work on board

--- statki.py
+++ statki.py
@@ -1,7 +1,12 @@
-# plansza, walidacja
-# statki, komentarz kiedy jest zatopiony, życie/rozmiar
-# strzaly
-# zasady umiejscowie, strzałow, kiedy sie konczy,
+# statki: 2,3,4 polowe. dodanie wielkosci i kierunku gdzie go postawic. sprawdzenie czy da sie go postawic
+# strzały: wybranie pola podanego przez gracza i sprawdzenie czy jest tam statek
+# zasady gry: gracz ma 10 strzałow, jesli trafi w statek ma kolejny strzal, jesli nie trafi traci 1 zycie
+# jesli bezdie to statek 1 polowy, przy pierwszym strzale jest zatopiony
+# reszta musi dac jakis znak ze nie jest zatopiona i dalej zyje a jak sie zatopi to tez jakis znak
+# plansza wyswietlenie: musi byc cala druga plansza pusta i gracz ma strzelac w nia, po strzale wyswietla pole czy jest
+# trafione czy nie + wyswietla ilosc pozostalychstrzalow
+
+
 import random
 
 tab = []
@@ -78,26 +83,111 @@ def free_place():
     return False
 
 
+def choose_random():
+    x = random.randint(0, 9)
+    y = random.randint(0, 9)
+    return x, y
+
+
+def ship_direction(x, y):
+    directions = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    random_cord = random.choice(directions)
+    if random_cord == (x - 1, y):
+        return 'Down'
+    if random_cord == (x + 1, y):
+        return 'Up'
+    if random_cord == (x, y - 1):
+        return 'Left'
+    if random_cord == (x, y + 1):
+        return 'Right'
+
+
 def get_ships():
-    for i in range(15):
+
+    for i in range(4):
         if free_place():
-            x = random.randint(0, 9)
-            y = random.randint(0, 9)
+            x, y = choose_random()
             while not is_pos_valid(x, y):
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
+                x, y = choose_random()
             tab[x][y] = '# '
 
+    for i in range(3):
+        x, y = choose_random()
+        random_cord = ship_direction(x, y)
+        if random_cord == 'Down':
+            while not is_pos_valid(x, y) or not is_pos_valid(x - 1, y):
+                x, y = choose_random()
 
-    # i = 0
-    # while i < 3:
-    #     x = LETTERS[random.randint(0, 9)]
-    #     y = random.randint(0, 9)
-    #     result = ships(x, y, 2, 'up')
-    #     if result:
-    #         i += 1
+            tab[x][y] = '# '
+            tab[x - 1][y] = '# '
+        if random_cord == "Up":
+            while not is_pos_valid(x, y) or not is_pos_valid(x + 1, y):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x + 1][y] = '# '
+        if random_cord == "Left":
+            while not is_pos_valid(x, y) or not is_pos_valid(x, y - 1):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x][y - 1] = '# '
+        if random_cord == "Right":
+            while not is_pos_valid(x, y) or not is_pos_valid(x, y + 1):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x][y + 1] = '# '
+
+    for i in range(2):
+        x, y = choose_random()
+        random_cord = ship_direction(x, y)
+        if random_cord == 'Down':
+            while not is_pos_valid(x, y) or not is_pos_valid(x - 1, y) or not is_pos_valid(x - 2, y):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x - 1][y] = '# '
+            tab[x - 2][y] = '# '
+        if random_cord == "Up":
+            while not is_pos_valid(x, y) or not is_pos_valid(x + 1, y) or not is_pos_valid(x + 2, y):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x + 1][y] = '# '
+            tab[x + 2][y] = '# '
+        if random_cord == "Left":
+            while not is_pos_valid(x, y) or not is_pos_valid(x, y - 1) or not is_pos_valid(x, y - 2):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x][y - 1] = '# '
+            tab[x][y - 2] = '# '
+        if random_cord == "Right":
+            while not is_pos_valid(x, y) or not is_pos_valid(x, y + 1) or not is_pos_valid(x, y + 2):
+                x, y = choose_random()
+
+            tab[x][y] = '# '
+            tab[x][y + 1] = '# '
+            tab[x][y + 2] = '# '
+
+    # up = x - 1, y
+    # down= x + 1, y
+    # right = x, y + 1
+    # left = x, y - 1
     #
-    # i = 0
+    # up = x - 1, y, x - 2, y
+    # down = x + 1, y, x + 2, y,
+    # right = x, y + 1, right = x, y + 2
+    # left = x, y - 1, x, y - 2,
+    #
+    # up = x - 1, y up = x - 2, y up = x - 3, y
+    # down = x + 1, y  down = x + 2, y  down = x + 3, y
+    # right = x, y + 1 right = x, y + 2 right = x, y + 3
+    # left = x, y - 1 left = x, y - 2 left = x, y - 3
+
+
+
     # while i < 2:
     #     x = LETTERS[random.randint(0, 9)]
     #     y = random.randint(0, 9)
